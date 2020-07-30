7b06af20d0ace756914665d5228bfd304140e270
Steve Blamey <sblamey@uzerp.com>
2020-07-30 12:40:26 +0100

    Clean-up scripts

--- uzerp/scripts/build-pod.sh
+++ uzerp/scripts/build-pod.sh
@@ -5,23 +5,26 @@ LOCALIP=$1
 UZERP_SOURCE_DIR=$2
 XDEBUG_CONFIG="remote_host=${LOCALIP} remote_port=9000 remote_log=/var/xdebug.log"
 POSTGRES_PASSWORD=xxx
-APPARMOR_OPT=""
-
-if [ -d "/sys/kernel/security/apparmor" ]; then
-    APPARMOR_OPT="--security-opt apparmor=unconfined"
-fi
+TZ="Europe/London"
 
 podman pod create --name uzerp-pod -p 5432 -p 9187 -p 8080:80 -p 8085:5000
-podman run --pod uzerp-pod --name uzerp-postgres -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -d quay.io/uzerp/uzerp-postgres
-podman run --pod uzerp-pod --name uzerp-memcache -d memcached
+podman run -d --pod uzerp-pod --name uzerp-postgres --security-opt label=disable \
+-v ${XDG_CONFIG_HOME:-$HOME/.config}/uzerp/postgres/postgres.conf:/etc/postgresql/postgresql.conf \
+-v ${XDG_CONFIG_HOME:-$HOME/.config}/uzerp/postgres/data:/var/lib/postgresql/data \
+-e TZ=$TZ -e PGTZ=$TZ \
+-e POSTGRES_PASSWORD=$POSTGRES_PASSWORD quay.io/uzerp/uzerp-postgres \
+-c "config_file=/etc/postgresql/postgresql.conf"
 
-podman run --pod uzerp-pod --name uzerp-app-dev --security-opt label=disable $APPARMOR_OPT \
+podman run --pod uzerp-pod --name uzerp-memcache -e TZ=$TZ -d memcached
+
+podman run --pod uzerp-pod --name uzerp-app-dev --security-opt label=disable \
 -v $UZERP_SOURCE_DIR:/var/www/html:rw \
---env XDEBUG_CONFIG="${XDEBUG_CONFIG}" \
+--env XDEBUG_CONFIG="${XDEBUG_CONFIG}" -e TZ=$TZ \
 -d quay.io/uzerp/uzerp-app-dev
 
-podman run --pod uzerp-pod --name uzerp-frepple --security-opt label=disable $APPARMOR_OPT \
+podman run --pod uzerp-pod --name uzerp-frepple --security-opt label=disable \
 -v ${XDG_CONFIG_HOME:-$HOME/.config}/uzerp/frepple/logs:/app/frepple/logs:rw \
 -v ${XDG_CONFIG_HOME:-$HOME/.config}/uzerp/frepple/etc:/etc/frepple:ro \
+-e TZ=$TZ \
 -d quay.io/uzerp/uzerp-frepple
 
--- uzerp/scripts/pg-bootstrap.sh
+++ uzerp/scripts/pg-bootstrap.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
-podman exec -i uzerp-postgres psql -U postgres -c "create user \"www-data\"  with encrypted password 'xxx';"
-podman exec -i uzerp-postgres psql -U postgres -c "create user sysadmin  with encrypted password 'sysadmin';"
-podman exec -i uzerp-postgres psql -U postgres -c "ALTER USER sysadmin WITH SUPERUSER;"
-podman exec -i uzerp-postgres psql -U postgres -c "create user \"ooo-data\"  with encrypted password 'data123';"
-podman exec -i uzerp-postgres psql -U postgres -c "create user readonly  with encrypted password 'data123';"
+if [ "$( podman exec -i uzerp-postgres psql -U postgres -tAc "SELECT 1 FROM pg_database WHERE datname='uzerp'" )" = '1' ]
+then
+    >&2 echo "Database already exists"
+else
+    podman exec -i uzerp-postgres psql -U postgres -c "create user \"www-data\"  with encrypted password 'xxx';"
+    podman exec -i uzerp-postgres psql -U postgres -c "create user sysadmin  with encrypted password 'sysadmin';"
+    podman exec -i uzerp-postgres psql -U postgres -c "ALTER USER sysadmin WITH SUPERUSER;"
+    podman exec -i uzerp-postgres psql -U postgres -c "create user \"ooo-data\"  with encrypted password 'data123';"
+    podman exec -i uzerp-postgres psql -U postgres -c "create user readonly  with encrypted password 'data123';"
 
-# Create demo database
-podman exec -i uzerp-postgres psql -U postgres -c 'create database uzerp;'
-podman exec -i uzerp-postgres pg_restore -U postgres --dbname=uzerp < $1/schema/database/postgresql/uzerp-demo-dist.sql
-# Migrate using phinx
-podman exec -i uzerp-app-dev php vendor/bin/phinx migrate -e development
+    # Create demo database
+    podman exec -i uzerp-postgres psql -U postgres -c 'create database uzerp;'
+    podman exec -i uzerp-postgres pg_restore -U postgres --dbname=uzerp < $1/schema/database/postgresql/uzerp-demo-dist.sql
+    # Migrate using phinx
+    podman exec -i uzerp-app-dev php vendor/bin/phinx migrate -e development
 
-# Create frepple database
-podman exec -i uzerp-postgres psql -U postgres -c "create user frepple with encrypted password 'frepple';"
-podman exec -i uzerp-postgres psql -U postgres -c 'create database frepple;'
-podman exec -i uzerp-postgres psql -U postgres -c "ALTER DATABASE frepple OWNER TO frepple;"
-podman exec -i uzerp-postgres psql -U postgres -c "ALTER ROLE frepple WITH CREATEROLE;"
-# Run frepple migrations
-podman exec uzerp-frepple ./frepplectl.py migrate
-# Add uzerp-frepple schema and grants to uzERP database
-podman exec -i uzerp-postgres psql -U postgres --dbname=uzerp < ./frepple-schema.sql
-podman exec -i uzerp-postgres psql -U postgres --dbname=uzerp < ./frepple-grants.sql
-
+    # Create frepple database
+    podman exec -i uzerp-postgres psql -U postgres -c "create user frepple with encrypted password 'frepple';"
+    podman exec -i uzerp-postgres psql -U postgres -c 'create database frepple;'
+    podman exec -i uzerp-postgres psql -U postgres -c "ALTER DATABASE frepple OWNER TO frepple;"
+    podman exec -i uzerp-postgres psql -U postgres -c "ALTER ROLE frepple WITH CREATEROLE;"
+    # Run frepple migrations
+    podman exec uzerp-frepple ./frepplectl.py migrate
+    # Add uzerp-frepple schema and grants to uzERP database
+    podman exec -i uzerp-postgres psql -U postgres --dbname=uzerp < ./frepple-schema.sql
+    podman exec -i uzerp-postgres psql -U postgres --dbname=uzerp < ./frepple-grants.sql
+fi
