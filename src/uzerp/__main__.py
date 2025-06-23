bc7877b3832a6f7c1a45e11dad311addd1b41e43
Steve Blamey <sblamey@uzerp.com>
2025-06-23 16:58:55 +0100

    fix(format): indentation to 4sp

--- src/uzerp/__main__.py
+++ src/uzerp/__main__.py
@@ -7,95 +7,95 @@ from uzerp.pod import Pod
 
 
 class cli(object):
-  """
-  uzERP podman pod control.
-  """
-  def __init__(self):
-    self.our_pod = Pod()
-
-  def up(self, ip='127.0.0.1'):
     """
-    Start the uzERP container pod
-
-    Starts the pod using podman or creates one, if it doesn't exist.
-    After creating the pod the postgres users and databases are created.
-
-    :param ip: a local ip address for Xdebug connections (uses port 9000).
-    WARNING: This must be a real IP, localhost will not work.
+    uzERP podman pod control.
     """
-    self.our_pod.up(ip)    
-    print(self.our_pod.getstatus())
+    def __init__(self):
+        self.our_pod = Pod()
 
-  def debugip(self, ip):
-    """Change the Xdebug remote IP address
-    
-    :param ip: a local ip address for Xdebug connections (uses port 9000).
-    WARNING: This must be a real IP, localhost will not work.
-    """
-    self.our_pod.xdebugip(ip)
+    def up(self, ip='127.0.0.1'):
+        """
+        Start the uzERP container pod
 
-  def halt(self):
-    """Stop the uzERP container pod."""
-    self.stop()
+        Starts the pod using podman or creates one, if it doesn't exist.
+        After creating the pod the postgres users and databases are created.
 
-  def stop(self):
-    """Stop the uzERP container pod."""
-    cprint('Stopping pod...', 'green')
-    self.our_pod.stop()
-    print(self.our_pod.getstatus())
+        :param ip: a local ip address for Xdebug connections (uses port 9000).
+        WARNING: This must be a real IP, localhost will not work.
+        """
+        self.our_pod.up(ip)
+        print(self.our_pod.getstatus())
 
-  def destroy(self):
-    """Remove the uzERP container pod."""
-    if self.our_pod.getstatus():
-      cprint('Removing pod...', 'green')
-      self.our_pod.remove()
-      print('Done.')
+    def debugip(self, ip):
+        """Change the Xdebug remote IP address
+        
+        :param ip: a local ip address for Xdebug connections (uses port 9000).
+        WARNING: This must be a real IP, localhost will not work.
+        """
+        self.our_pod.xdebugip(ip)
 
-  def status(self):
-    """Show the current status of the uzERP pod."""
-    print(self.our_pod.getstatus())
+    def halt(self):
+        """Stop the uzERP container pod."""
+        self.stop()
+
+    def stop(self):
+        """Stop the uzERP container pod."""
+        cprint('Stopping pod...', 'green')
+        self.our_pod.stop()
+        print(self.our_pod.getstatus())
+
+    def destroy(self):
+        """Remove the uzERP container pod."""
+        if self.our_pod.getstatus():
+            cprint('Removing pod...', 'green')
+            self.our_pod.remove()
+            print('Done.')
+
+    def status(self):
+        """Show the current status of the uzERP pod."""
+        print(self.our_pod.getstatus())
 
 
-  class db(object):
-    """Database operations"""
-    def migrate():
-      """Run phinx migrations"""
-      subprocess.run(["podman", "exec", "-i",  "uzerp-app-dev", "php", "vendor/bin/phinx", "migrate", "-e", "development"])
+    class db(object):
+        """Database operations"""
+        def migrate():
+            """Run phinx migrations"""
+            subprocess.run(["podman", "exec", "-i", "uzerp-app-dev", "php", "vendor/bin/phinx", "migrate", "-e", "development"])
 
-    def rollback():
-      """Roll back phinx migrations"""
-      subprocess.run(["podman", "exec", "-i",  "uzerp-app-dev", "php", "vendor/bin/phinx", "rollback", "-e", "development"])
+        def rollback():
+            """Roll back phinx migrations"""
+            subprocess.run(["podman", "exec", "-i", "uzerp-app-dev", "php", "vendor/bin/phinx", "rollback", "-e", "development"])
 
-    def newmigration(migration_name):
-      """Create new phinx migration"""
-      subprocess.run(["podman", "exec", "-i",  "uzerp-app-dev", "php", "vendor/bin/phinx", "create", migration_name])
+        def newmigration(migration_name):
+            """Create new phinx migration"""
+            subprocess.run(["podman", "exec", "-i", "uzerp-app-dev", "php", "vendor/bin/phinx", "create", migration_name])
 
-  class composer(object):
-    """Composer commands"""
-    def install():
-      """install php dependencies"""
-      subprocess.run(["podman", "exec", "-i",  "uzerp-app-dev", "composer", "install"])
+    class composer(object):
+        """Composer commands"""
+        def install():
+            """install php dependencies"""
+            subprocess.run(["podman", "exec", "-i", "uzerp-app-dev", "composer", "install"])
 
-    def update():
-      """update php dependencies"""
-      subprocess.run(["podman", "exec", "-i",  "uzerp-app-dev", "composer", "update"])
+        def update():
+            """update php dependencies"""
+            subprocess.run(["podman", "exec", "-i", "uzerp-app-dev", "composer", "update"])
 
 def main():
-  c = cli()
-  fire.Fire({'up': c.up,
-    'debugip': c.debugip,
-    'status': c.status,
-    'stop': c.stop,
-    'halt': c.halt,
-    'destroy': c.destroy,
-    'db': {
-      'migrate': c.db.migrate,
-      'rollback': c.db.rollback,
-      'newmigration': c.db.newmigration},
-    'composer': {
-      'install': c.composer.install,
-      'update': c.composer.update}
-  })
+    c = cli()
+    fire.Fire({'up': c.up,
+        'debugip': c.debugip,
+        'status': c.status,
+        'stop': c.stop,
+        'halt': c.halt,
+        'destroy': c.destroy,
+        'db': {
+            'migrate': c.db.migrate,
+            'rollback': c.db.rollback,
+            'newmigration': c.db.newmigration},
+        'composer': {
+            'install': c.composer.install,
+            'update': c.composer.update}
+    })
 
 if __name__ == '__main__':
-  main()
+    main()
--- src/uzerp/pod.py
+++ src/uzerp/pod.py
@@ -8,99 +8,99 @@ from xdg import XDG_CONFIG_HOME
 from xdg import XDG_DATA_HOME
 
 class Pod(object):
-  """
-  Podman Pod
-  """
-  def __init__(self):
-    self.update()
-
-  def update(self, pod_name='uzerp-pod', uzerp_file=Path(os.path.join(os.getcwd(), "phinx.yml"))):
-    """Update the status of the pod"""
-    pod = subprocess.run(["podman",  "pod",  "ps", '--format="{{.Name}}/{{.ID}}/{{.Status}}"', "--filter=name={}".format(pod_name)], capture_output=True)
-    podinfo = pod.stdout.decode("utf-8").replace('"', '').replace('\n', '').split('/')
-    self._name = None
-    self._id = None
-    self._status = None
-    self.uzerp_file = uzerp_file
-    if podinfo[0] != "":
-      self._name = podinfo[0]
-      self._id = podinfo[1]
-      self._status = podinfo[2]
-
-  def getstatus(self):
-    """Return the current status of the pod"""
-    self.update()
-    return self._status
-
-  def provision(self, ip):
     """
-    Call the pod build script and bootstrap the databases.
-    
-    The ip argument is the address for Xdebug connections,
-    usually this is an address on the local PC.
-
-    It is assumed that the current working directory contains
-    the uzERP source code that will be bind mounted to the container.
+    Podman Pod
     """
-    script_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "scripts")
-    working_dir = os.getcwd()
+    def __init__(self):
+        self.update()
 
-    Path(os.path.join(XDG_DATA_HOME, "uzerp", "postgres", "data")).mkdir(parents=True, exist_ok=True)
-    Path(os.path.join(XDG_CONFIG_HOME, "uzerp", "postgres")).mkdir(parents=True, exist_ok=True)
-    shutil.copyfile(os.path.join(script_dir, "postgres.conf"), os.path.join(XDG_CONFIG_HOME, "uzerp", "postgres", "postgres.conf"))
+    def update(self, pod_name='uzerp-pod', uzerp_file=Path(os.path.join(os.getcwd(), "phinx.yml"))):
+        """Update the status of the pod"""
+        pod = subprocess.run(["podman", "pod", "ps", '--format="{{.Name}}/{{.ID}}/{{.Status}}"', "--filter=name={}".format(pod_name)], capture_output=True)
+        podinfo = pod.stdout.decode("utf-8").replace('"', '').replace('\n', '').split('/')
+        self._name = None
+        self._id = None
+        self._status = None
+        self.uzerp_file = uzerp_file
+        if podinfo[0] != "":
+            self._name = podinfo[0]
+            self._id = podinfo[1]
+            self._status = podinfo[2]
 
-    if not self.uzerp_file.is_file():
-      cprint("Provisioning needed: please run this command in the top-level directory of uzERP source code", 'red')
-      exit(1)
-    cprint('Creating containers/volumes and adding to pod...', 'green')
-    subprocess.run([os.path.join(script_dir, "build-pod.sh"), ip, working_dir], cwd=script_dir)
-    cprint('\nBootstrapping postgres databases...', 'green')
-    time.sleep(5)
-    subprocess.run([os.path.join(script_dir, "pg-bootstrap.sh"), working_dir], cwd=script_dir, stdout=subprocess.DEVNULL)
-    cprint('\nuzERP pod ready at http://localhost:8080', 'green')
-    
-  def xdebugip(self, ip):
-    """
-    Change the Xdebug remote IP address
+    def getstatus(self):
+        """Return the current status of the pod"""
+        self.update()
+        return self._status
 
-    Remove the uzERP app container an recreate it with a new Xdebug configuration.
+    def provision(self, ip):
+        """
+        Call the pod build script and bootstrap the databases.
+        
+        The ip argument is the address for Xdebug connections,
+        usually this is an address on the local PC.
 
-    It is assumed that the current working directory contains
-    the uzERP source code that will be bind mounted to the container.
-    """
-    if not self.uzerp_file.is_file():
-      cprint("Provisioning needed: please run this command in the top-level directory of uzERP source code", 'red')
-      exit(1)
-    find_app_container = subprocess.run(["podman",  "ps", '--filter', 'name=uzerp-app-dev', '--format="{{.ID}}"'], capture_output=True)
-    app_container = find_app_container.stdout.decode("utf-8").replace('"', '').replace('\n', '')
-    cprint('Replacing app container...', 'green')
-    add_container = subprocess.run(["podman", "run",  "--replace", "--pod", self._name,
-                                    "--name", "uzerp-app-dev",
-                                    "--security-opt", "label=disable",
-                                    "-v", "{}:/var/www/html:rw".format(os.getcwd()),
-                                    "--env", 'XDEBUG_CONFIG=client_host={} client_port=9000 log=/tmp/xdebug.log'.format(ip),
-                                    "--env", "XDEBUG_MODE=debug",
-                                    "--env", "TZ=Europe/London",
-                                    '-d', 'ghcr.io/uzerpllp/uzerp-app-dev'], capture_output=True)
+        It is assumed that the current working directory contains
+        the uzERP source code that will be bind mounted to the container.
+        """
+        script_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "scripts")
+        working_dir = os.getcwd()
 
-  def up(self, ip):
-    """
-    Start the pod
-    """
-    if self._name is None:
-      self.provision(ip)
-    elif self._name and (self._status == "Stopped" or self._status == "Exited" or self._status == "Created"):
-      subprocess.run(["podman", "pod", "start", self._name])
-    self.update()
+        Path(os.path.join(XDG_DATA_HOME, "uzerp", "postgres", "data")).mkdir(parents=True, exist_ok=True)
+        Path(os.path.join(XDG_CONFIG_HOME, "uzerp", "postgres")).mkdir(parents=True, exist_ok=True)
+        shutil.copyfile(os.path.join(script_dir, "postgres.conf"), os.path.join(XDG_CONFIG_HOME, "uzerp", "postgres", "postgres.conf"))
 
-  def stop(self):
-    """Stop the pod"""
-    if self._status == "Running":
-      subprocess.run(["podman", "pod", "stop", self._name])
-    self.update()
+        if not self.uzerp_file.is_file():
+            cprint("Provisioning needed: please run this command in the top-level directory of uzERP source code", 'red')
+            exit(1)
+        cprint('Creating containers/volumes and adding to pod...', 'green')
+        subprocess.run([os.path.join(script_dir, "build-pod.sh"), ip, working_dir], cwd=script_dir)
+        cprint('\nBootstrapping postgres databases...', 'green')
+        time.sleep(5)
+        subprocess.run([os.path.join(script_dir, "pg-bootstrap.sh"), working_dir], cwd=script_dir, stdout=subprocess.DEVNULL)
+        cprint('\nuzERP pod ready at http://localhost:8080', 'green')
+        
+    def xdebugip(self, ip):
+        """
+        Change the Xdebug remote IP address
 
-  def remove(self):
-    """Remove the pod"""
-    if self._name:
-      subprocess.run(["podman", "pod", "rm", self._name, "--force"])
-    self.update()
+        Remove the uzERP app container an recreate it with a new Xdebug configuration.
+
+        It is assumed that the current working directory contains
+        the uzERP source code that will be bind mounted to the container.
+        """
+        if not self.uzerp_file.is_file():
+            cprint("Provisioning needed: please run this command in the top-level directory of uzERP source code", 'red')
+            exit(1)
+        find_app_container = subprocess.run(["podman", "ps", '--filter', 'name=uzerp-app-dev', '--format="{{.ID}}"'], capture_output=True)
+        app_container = find_app_container.stdout.decode("utf-8").replace('"', '').replace('\n', '')
+        cprint('Replacing app container...', 'green')
+        add_container = subprocess.run(["podman", "run", "--replace", "--pod", self._name,
+                                        "--name", "uzerp-app-dev",
+                                        "--security-opt", "label=disable",
+                                        "-v", "{}:/var/www/html:rw".format(os.getcwd()),
+                                        "--env", 'XDEBUG_CONFIG=client_host={} client_port=9000 log=/tmp/xdebug.log'.format(ip),
+                                        "--env", "XDEBUG_MODE=debug",
+                                        "--env", "TZ=Europe/London",
+                                        '-d', 'ghcr.io/uzerpllp/uzerp-app-dev'], capture_output=True)
+
+    def up(self, ip):
+        """
+        Start the pod
+        """
+        if self._name is None:
+            self.provision(ip)
+        elif self._name and (self._status == "Stopped" or self._status == "Exited" or self._status == "Created"):
+            subprocess.run(["podman", "pod", "start", self._name])
+        self.update()
+
+    def stop(self):
+        """Stop the pod"""
+        if self._status == "Running":
+            subprocess.run(["podman", "pod", "stop", self._name])
+        self.update()
+
+    def remove(self):
+        """Remove the pod"""
+        if self._name:
+            subprocess.run(["podman", "pod", "rm", self._name, "--force"])
+        self.update()
